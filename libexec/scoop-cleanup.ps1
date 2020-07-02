67baaec874fa2c15df0229d1761eca56272cf042
jingyu9575 <qw2g64@outlook.com>
2020-07-02 22:48:30 +0800

    Change scoop-cleanup function

--- libexec/scoop-cleanup.ps1
+++ libexec/scoop-cleanup.ps1
@@ -1,13 +1,13 @@
 # Usage: scoop cleanup <app> [options]
-# Summary: Cleanup apps by removing old versions
-# Help: 'scoop cleanup' cleans Scoop apps by removing old versions.
-# 'scoop cleanup <app>' cleans up the old versions of that app if said versions exist.
+# Summary: Cleanup apps by removing old caches
+# Help: 'scoop cleanup <app>' cleans up the old caches of that app.
 #
 # You can use '*' in place of <app> to cleanup all apps.
 #
 # Options:
 #   -g, --global       Cleanup a globally installed app
 #   -k, --cache        Remove outdated download cache
+#   -t, --temp         Remove temporary files
 
 . "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\manifest.ps1"
@@ -19,12 +19,11 @@
 
 reset_aliases
 
-$opt, $apps, $err = getopt $args 'gk' 'global', 'cache'
+$opt, $apps, $err = getopt $args 'gkt' 'global', 'cache', 'temp'
 if ($err) { "scoop cleanup: $err"; exit 1 }
 $global = $opt.g -or $opt.global
 $cache = $opt.k -or $opt.cache
-
-if (!$apps) { 'ERROR: <app> missing'; my_usage; exit 1 }
+$temp = $opt.t -or $opt.temp
 
 if ($global -and !(is_admin)) {
     'ERROR: you need admin rights to cleanup global apps'; exit 1
@@ -35,46 +34,42 @@ function cleanup($app, $global, $verbose, $cache) {
     if ($cache) {
         Remove-Item "$cachedir\$app#*" -Exclude "$app#$current_version#*"
     }
-    $versions = versions $app $global | Where-Object { $_ -ne $current_version -and $_ -ne 'current' }
-    if (!$versions) {
-        if ($verbose) { success "$app is already clean" }
-        return
-    }
-
-    write-host -f yellow "Removing $app`:" -nonewline
-    $versions | ForEach-Object {
-        $version = $_
-        write-host " $version" -nonewline
-        $dir = versiondir $app $version $global
-        # unlink all potential old link before doing recursive Remove-Item
-        unlink_persist_data $dir
-        Remove-Item $dir -ErrorAction Stop -Recurse -Force
-    }
-    write-host ''
 }
 
-if ($apps) {
-    $verbose = $true
-    if ($apps -eq '*') {
-        $verbose = $false
-        $apps = applist (installed_apps $false) $false
-        if ($global) {
-            $apps += applist (installed_apps $true) $true
+$verbose = $true
+if ($apps -eq '*') {
+    $verbose = $false
+    $apps = applist (installed_apps $false) $false
+    if ($global) {
+        $apps += applist (installed_apps $true) $true
+    }
+} elseif ($apps) {
+    $apps = Confirm-InstallationStatus $apps -Global:$global
+}
+
+# $apps is now a list of ($app, $global) tuples
+$apps | ForEach-Object { cleanup @_ $verbose $cache }
+
+if ($cache) {
+    Remove-Item "$cachedir\*.download" -ErrorAction Ignore
+}
+
+if ($temp) {
+    $tempdir = tempdir $global
+    if (Test-Path $tempdir) {
+        Get-ChildItem $tempdir | ForEach-Object {
+            $item = $_.FullName
+            try {
+                Remove-Item $item -Recurse -Force -ErrorAction Stop
+            } catch {
+                warn "Couldn't remove '$(friendly_path $item)'; it may be in use."
+            }
         }
-    } else {
-        $apps = Confirm-InstallationStatus $apps -Global:$global
     }
+}
 
-    # $apps is now a list of ($app, $global) tuples
-    $apps | ForEach-Object { cleanup @_ $verbose $cache}
-
-    if ($cache) {
-        Remove-Item "$cachedir\*.download" -ErrorAction Ignore
-    }
-
-    if (!$verbose) {
-        success 'Everything is shiny now!'
-    }
+if (!$verbose) {
+    success 'Everything is shiny now!'
 }
 
 exit 0
